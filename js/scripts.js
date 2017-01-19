f0a607f5b3311da8dc33c6fd4337923988c1ea8b
Jennifer Beem and Katie Reed <jennifer@>
2017-01-18 16:54:57 -0800

    git it to work

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,17 @@ $(document).ready(function() {
   var vowel = ["a", "e", "i", "o", "u"];
   var cons = "bcdfghjklmnprstvwxyzBCDFGHJKLMNPRSTVWXYZ".split("");
   var vowelFinder = function(phraseArray) {
-    if (phraseArray[0] === vowel) {
-      return true;
-    } else {
-      return false;
+    for (var phraseIndex = 0; phraseIndex < phraseArray.length; phraseIndex +=1){
+      for (var vowelIndex = 0; vowelIndex < vowel.length; vowelIndex += 1) {
+        if (phraseArray[phraseIndex] === vowel[vowelIndex]) {
+          return true;
+        } else {
+          // return false;
+        };
+      };
     };
   };
+
 
   var consFinder = function(phraseArray) {
     if(phraseArray[0] === cons) {
@@ -19,14 +24,14 @@ $(document).ready(function() {
     };
   };
 
-  var pigLatin = function(phraseArray) {
-    if (vowelFinder === true) {
-      return phraseString + "ay"
-    //   $(".translation").text(phrase.concat("ay"));
-    //   $(".translation").show();
+  var pigLatin = function(phraseArray, phraseString) {
+    alert("hey!");
+    if (vowelFinder(phraseArray) === true) {
+        phraseString = phraseString + "ay"
+        return phraseString
     // } else if (consFinder === true) {
     //   (phraseArray[0,1,2].append()).concat("ay");
-    // }
+    };
   };
   $("form#piglatin").submit(function(event) {
   event.preventDefault();
@@ -34,10 +39,11 @@ $(document).ready(function() {
   var phraseArray = phraseString.split("");
   alert("phraseString: " + phraseString);
   alert("phraseArray: " + phraseArray);
-  $(".translation").text(pigLatin(phraseArray));
+  $(".translation").text(pigLatin(phraseArray, phraseString));
   $("#translation").show();
-  });
 });
+});
+
 
 
 
