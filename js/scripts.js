0397d954ac8eb0d4ec8d8bb0e7b2819887fb2b5d
Katie Reed <katharynreedpierce@gmail.com>
2017-01-18 18:59:58 -0800

    Adds comments to explain pigLatin function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,36 @@
 var vowels = ["a", "e", "i", "o", "u"];
 
 var pigLatin = function(phraseArray) {
+  // vowelsIndex is a positive number if first letter in phraseArray is a vowel
+  //Otherwise vowelsIndex is -1
   var vowelsIndex = vowels.indexOf(phraseArray[0]);
+
+  //If vowelsIndex is not -1, first letter in phraseArray IS a vowel, then..
   if (vowelsIndex !== -1) {
-    phraseArray.push(vowels[vowelsIndex]);
-    phraseArray.push("a");
-    phraseArray.shift();
-    return phraseArray;
+    phraseArray.push(vowels[vowelsIndex]); //push first letter (vowel) to end of array
+    phraseArray.push("a"); //add an 'a' to the end of the array
+    phraseArray.shift(); //remove first letter (vowel) from the beginning of array
+    return phraseArray; //return pigLatin word
   }
 
+  //If above doesn't return, then first letter is not a vowel
+  //For each letter in phraseArray...
   for (var i = 0; i < phraseArray.length; i++) {
+
+    //if the current letter is a vowel
     if(vowels.indexOf(phraseArray[0]) !== -1) {
-      phraseArray.push("a");
-      return phraseArray;
+      phraseArray.push("a"); //push an 'a' to the end of phraseArray
+      return phraseArray; //end loop by returning phraseArray
     }
 
-    debugger;
+    //Otherwise, push current first letter to the end of the array
     phraseArray.push(phraseArray[0]);
-    phraseArray.splice(0, 1);
+    phraseArray.splice(0, 1); //remove current first letter
   }
 
-  phraseArray.push("a");
-  return phraseArray;
+  //if the loops ends (when the 'if' on line 21 is True -- we found a vowel)
+  phraseArray.push("a"); //push an 'a' to the end of the array
+  return phraseArray; //return pigLatin word
 };
 
 $(document).ready(function() {
