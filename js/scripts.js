a679d0e46eeb824db48217cdfae01aca42187937
Katie Reed <katharynreedpierce@gmail.com>
2017-01-18 18:47:30 -0800

    Finishes pig latin conversion functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -1,59 +1,35 @@
+var vowels = ["a", "e", "i", "o", "u"];
+
+var pigLatin = function(phraseArray) {
+  var vowelsIndex = vowels.indexOf(phraseArray[0]);
+  if (vowelsIndex !== -1) {
+    phraseArray.push(vowels[vowelsIndex]);
+    phraseArray.push("a");
+    phraseArray.shift();
+    return phraseArray;
+  }
+
+  for (var i = 0; i < phraseArray.length; i++) {
+    if(vowels.indexOf(phraseArray[0]) !== -1) {
+      phraseArray.push("a");
+      return phraseArray;
+    }
+
+    phraseArray.push(phraseArray[i]);
+    phraseArray.splice(i, 1);
+  }
+  
+  phraseArray.push("a");
+  return phraseArray;
+};
+
 $(document).ready(function() {
-
-
-  var vowel = ["a", "e", "i", "o", "u"];
-  var cons = "bcdfghjklmnprstvwxyzBCDFGHJKLMNPRSTVWXYZ".split("");
-  var vowelFinder = function(phraseArray) {
-    for (var phraseIndex = 0; phraseIndex < phraseArray.length; phraseIndex +=1){
-      for (var vowelIndex = 0; vowelIndex < vowel.length; vowelIndex += 1) {
-        if (phraseArray[phraseIndex] === vowel[vowelIndex]) {
-          return true;
-        } else {
-          // return false;
-        };
-      };
-    };
-  };
-
-
-  var consFinder = function(phraseArray) {
-    if(phraseArray[0] === cons) {
-      return true;
-    } else {
-      return false;
-    };
-  };
-
-  var pigLatin = function(phraseArray, phraseString) {
-    alert("hey!");
-    if (vowelFinder(phraseArray) === true) {
-        phraseString = phraseString + "ay"
-        return phraseString
-    // } else if (consFinder === true) {
-    //   (phraseArray[0,1,2].append()).concat("ay");
-    };
-  };
   $("form#piglatin").submit(function(event) {
-  event.preventDefault();
-  var phraseString = $("input#phrase").val();
-  var phraseArray = phraseString.split("");
-  alert("phraseString: " + phraseString);
-  alert("phraseArray: " + phraseArray);
-  $(".translation").text(pigLatin(phraseArray, phraseString));
-  $("#translation").show();
+    event.preventDefault();
+    var phraseString = $("input#phrase").val();
+    var phraseArray = phraseString.split("");
+    var pigLatinWord = pigLatin(phraseArray).join("");
+    $(".translation").text(pigLatinWord);
+    $("#translation").show();
+  });
 });
-});
-
-
-
-
-
-
-// $(document).ready(function() {
-// $("form#piglatin").submit(function(event) {
-// event.preventDefault();
-// var phrase = ($("input#phrase"));
-// var translation = "help me";
-// $("#translation").show();
-// });
-// });
